819428a28a489f561e34653aa60f0b208268edce
MaximVaskov <vskvmxm@yandex.ru>
2018-06-24 12:23:42 +0300

    Добавления 24.06

--- js/pictures.js
+++ js/pictures.js
@@ -25,6 +25,21 @@ var getRandomElements = function (items) {
     return randomItems;
 };
 
+var generateComment = function () {
+    var randomComments = getRandomElements(comments);
+    return randomComments.join(' ');
+}
+
+var createRandomComments = function () {
+    var createRComments = Math.floor(Math.random()*4)+1
+    var commentCreate =[];
+    for (var i=1; i<=createRComments; i++) {
+      commentCreate.push(generateComment());
+    }
+    return commentCreate;
+}
+console.log(createRandomComments());
+
 var countPhotos = 26;
 var photos = [];
 
@@ -52,12 +67,85 @@ pictures.appendChild(fragment);
 
 //Большое изображение:
 
-var bigPicture = document.querySelector('.big-picture').classList.remove('hidden');
+
 var bigPictureLikes = document.querySelector('.likes-count').textContent=likesStat;
 var bigPictureImg = document.querySelector('.big-picture__img');
 bigPictureImg.querySelector('img').src=image.src;
+
 var bigPictureComments = document.querySelector('.comments-count').textContent=commentsCount;
+
+var createCommentElement = function(comment) {
+    var index = Math.floor(Math.random()*6)+1
+    var createElementLi = document.createElement('li');
+    createElementLi.classList.add('social__comment');
+    var socialImg = document.createElement('img');
+    socialImg.classList.add('social__picture');
+    socialImg.src= 'img/avatar-'+index+'.svg';
+    var commentP = document.createElement('p');
+    commentP.classList.add('social__text');
+    commentP.textContent = comment;
+}
+console.log (createCommentElement());
+var createCommentsMassive = function (comments) {
+    var documentFragment = document.createDocumentFragment();
+    for (var i=0; i<= comments.length; i++){
+    documentFragment.appendChild(createCommentElement(comments[i]));
+    }
+    return documentFragment;
+}
 var socialCaption = document.querySelector('.social__caption').textContent=randomElement(descriptions);
+document.querySelector('.social__text').textContent=randomElement(comments);
+var socialComments = document.querySelector('.social__comment');
 
 var hiddenComments = document.querySelector('.social__comment-count', '.social__loadmore').classList.add('.visually-hidden');
 
+//Открытие/Закрытие
+
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var bigPicture = document.querySelector('.big-picture');
+var smallPicture = document.querySelector('.picture__img');
+var pictureClose = bigPicture.querySelector('.big-picture__cancel');
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  bigPicture.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+smallPicture.addEventListener('click', function() {
+  openPopup();
+});
+
+smallPicture.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+pictureClose.addEventListener('click', function() {
+  closePopup();
+});
+
+pictureClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+//Загрузка фотографий
+var downloadImageLabel = document.querySelector('.img-upload__label');
+var imgUploadOverlay = document.querySelector('.img-upload__overlay');
+//imgUploadOverlay.classList.remove('hidden');
